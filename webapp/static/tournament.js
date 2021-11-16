dac96865cce06eab6fb5e5c4512dd7862739141a
root <root@DESKTOP-BRHCGLJ.localdomain>
2021-11-15 22:26:39 -0600

    My changes

--- webapp/static/tournament.js
+++ webapp/static/tournament.js
@@ -1,5 +1,23 @@
 function initialize() {
+    let tournamentID = document.getElementsByName('tournament_id')[0].content;
+    let url = getAPIBaseURL() + '/tournament/' + tournamentID;
 
+    fetch(url, {method: 'get'})
+
+    .then((response) => response.json())
+
+    .catch(function(error) {
+        console.log(error);
+    });
 }
 
 window.onload = initialize;
+
+
+function getAPIBaseURL() {
+    let baseURL = window.location.protocol
+                    + '//' + window.location.hostname
+                    + ':' + window.location.port
+                    + '/api';
+    return baseURL;
+}
